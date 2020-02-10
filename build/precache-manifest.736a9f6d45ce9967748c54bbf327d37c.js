35d85a5a57a0f2312d7f6d6d6010f548f5a126c5
Rob Greene <robert.greene@giant.ie>
2020-02-10 13:50:18 +0000

    Adding files for github pages

---
+++ build/precache-manifest.736a9f6d45ce9967748c54bbf327d37c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3731842e6435ce0773b54acf7dd3626d",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "26008a3f51d7d36219c9",
+    "url": "/monsters-rolodex/static/css/main.327abb90.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26008a3f51d7d36219c9",
+    "url": "/monsters-rolodex/static/js/main.bc5aa5b8.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
